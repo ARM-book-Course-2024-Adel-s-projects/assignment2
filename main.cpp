75e345f14451a87033adf3e265fed2ca771adf41
adelarja <adelarja@gmail.com>
2024-07-16 15:14:19 -0300

    Changed main

--- main.cpp
+++ main.cpp
@@ -3,13 +3,16 @@
 #include "serial_interface.h"
 #include <mbed.h>
 
+DigitalIn PIN_1(D0);
+
 RainGauge_t rainGauge = {
-    { true, DigitalIn(D0), UP, 0 },
+    { true, PIN_1, UP, 0 },
     0,
     {0}
 };
 
 int main() {
+    PIN_1.mode(PullUp);
     initRainGauge(&rainGauge);
     while (true) {
         updateRainMeasure(&rainGauge);
