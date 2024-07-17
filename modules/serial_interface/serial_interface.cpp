a7b7dcf86268a47cbdd9d6fe509668360b39d17b
adelarja <adelarja@gmail.com>
2024-07-17 00:03:03 -0300

    added more serial commands. One to configure date and time and another one to get the current date and time

--- modules/serial_interface/serial_interface.cpp
+++ modules/serial_interface/serial_interface.cpp
@@ -3,6 +3,10 @@
 static void availableCommands(void);
 static char receiveChar(void);
 static void writeSerial(const char*);
+static void configureDateAndtime(void);
+static void readString(char*, int);
+static void getCurrentDateAndTime(void);
+
 
 static UnbufferedSerial uartUsb(USBTX, USBRX, 115200);
 
@@ -13,9 +17,17 @@ void uartTask()
 
     receivedChar = receiveChar();
     switch (receivedChar) {
-    case 'R':
+    case '1':
         sprintf ( str, "Accumulated tips: %d\r\n", getAccumulatedRain());
         writeSerial(str);
+        break;
+    
+    case '2':
+        configureDateAndtime();
+        break;
+    
+    case '3':
+        getCurrentDateAndTime();
         break;
 
     case '\0':
@@ -44,9 +56,54 @@ static char receiveChar() {
     return receivedChar;
 }
 
+static void configureDateAndtime() {
+    char year[5] = "";
+    char month[3] = "";
+    char day[3] = "";
+    char hour[3] = "";
+    char minute[3] = "";
+    char second[3] = "";
+
+    writeSerial("Configuring date and time: ");
+    
+    writeSerial("Type 4 digits for the year (YYYY):");
+    readString(&year, 4);
+
+    writeSerial("Type 2 digits for the month (MM):");
+    readString(&month, 3);
+
+    writeSerial("Type 2 digits for the day (DD):");
+    readString(&day, 3);
+
+    writeSerial("Type 2 digits for the minute (mm):");
+    readString(&minute, 3);
+
+    writeSerial("Type 2 digits for the second (ss):");
+    readString(&second, 3);
+
+    setDateAndTime( atoi(year), atoi(month), atoi(day), 
+        atoi(hour), atoi(minute), atoi(second) );
+
+    writeSerial("Date and Time configured!");
+}
+
 static void availableCommands()
 {
     uartUsb.write( "\n", 1);
     uartUsb.write( "Available commands:\r\n", 21 );
     uartUsb.write( "Press 'R' to get the accumulated tips of the tipping bucket raing gauge.\r\n", 48 );
+}
+
+static void readString(char* str, int strLength) {
+    for(int strIndex = 0; strIndex < strLength; strIndex++) {
+        uartUsb.read(&str[strIndex], 1);
+        uartUsb.write(&str[strIndex], 1);
+    }
+    str[strLength] = '\0';
+}
+
+static void getCurrentDateAndTime() {
+    char str[100] = "";
+    str = getDateAndTime();
+    writeSerial(str);
 }
